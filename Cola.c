547c57b724cb9976c68741528fc3dee65147e61e
gonzalo90 <gonzalospiojos_63@hotmail.com>
2014-02-13 12:43:37 -0200

    Update Cola.c

--- Cola.c
+++ Cola.c
@@ -1,1 +1,23 @@
-#define
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_nodo
+{
+  t_dato dato;
+  struct s_nodo *sig;
+}t_nodo;
+
+typedef struct
+{
+  t_nodo *pri;
+  t_nodo *ult;
+}t_cola;
+
+void crearCola(t_cola *);
+int ponerEnCola(t_cola *, t_dato *);
+int quitarDeCola(t_cola *, t_dato *);
+void vaciarCola(t_cola *);
+int colaVacia(t_cola *);
+int colaLlena(t_cola *);
+
+
